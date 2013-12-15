4e5dc958cbd9f8af1acad99f7883a22329ca36fe
Hunter-K13 <hunterk13mc@gmail.com>
2013-12-14 21:01:36 -0500

    .

--- hunterk13/ninjasecrets/items/ItemNinjaSkillSuspension.java
+++ hunterk13/ninjasecrets/items/ItemNinjaSkillSuspension.java
@@ -19,58 +19,38 @@ public class ItemNinjaSkillSuspension extends ItemNinjaSkillBase {
 		setMaxDamage(1800);
 	}
 	
-	public void onCreated(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
-		NBTTagCompound tag = getTagCompound(par1ItemStack);
-		tag.setInteger("oldFood", -987);
-		tag.setLong("time", -987);
-        par1ItemStack.setTagCompound(tag);
-	}
-	
 	@Override
 	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
     {
-		NBTTagCompound tag = par1ItemStack.getTagCompound();
-		/*this.inUse = true;
-		if (par1ItemStack.getItemDamage() < 500) {
-			System.out.println("2.1");
-			this.activated = true;
-		}*/
-		if (par2World.getTotalWorldTime() - tag.getLong("time") > 10) {
-			tag.setInteger("oldFood", -987);
-		}
-		if (par1ItemStack.getItemDamage() < par1ItemStack.getMaxDamage()/* && this.activated == true*/) {
-			par3EntityPlayer.addPotionEffect(new PotionEffect(13, 10, 0, true));
-			//par3EntityPlayer.addPotionEffect(new PotionEffect(8, 10, 127, true));
-			par3EntityPlayer.addPotionEffect(new PotionEffect(2, 10, 2, true));
-			if(tag.getInteger("oldFood") != -987) {
-				par3EntityPlayer.getFoodStats().setFoodLevel(tag.getInteger("oldFood"));
+		if (!par3EntityPlayer.capabilities.isCreativeMode) {
+			NBTTagCompound tag = getTagCompound(par1ItemStack);
+			/*this.inUse = true;
+			if (par1ItemStack.getItemDamage() < 500) {
+				System.out.println("2.1");
+				this.activated = true;
+			}*/
+			if (par2World.getTotalWorldTime() - tag.getLong("time") > 10) {
+				tag.setInteger("oldFood", -987);
 			}
-			System.out.println("2");
-			par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() + 1);
-			tag.setInteger("oldFood", par3EntityPlayer.getFoodStats().getFoodLevel());
-		}else if ((par1ItemStack.getMaxDamage() - par1ItemStack.getItemDamage()) <= 0) {
-			//this.activated = false;
-			System.out.println("3");
-			par1ItemStack.stackSize--;
+			if (par1ItemStack.getItemDamage() < par1ItemStack.getMaxDamage()/* && this.activated == true*/) {
+				par3EntityPlayer.addPotionEffect(new PotionEffect(13, 10, 0, true));
+				//par3EntityPlayer.addPotionEffect(new PotionEffect(8, 10, 127, true));
+				par3EntityPlayer.addPotionEffect(new PotionEffect(2, 10, 2, true));
+				if(tag.getInteger("oldFood") != -987) {
+					par3EntityPlayer.getFoodStats().setFoodLevel(tag.getInteger("oldFood"));
+				}
+				System.out.println("2");
+				par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() + 1);
+				tag.setInteger("oldFood", par3EntityPlayer.getFoodStats().getFoodLevel());
+			}else if ((par1ItemStack.getMaxDamage() - par1ItemStack.getItemDamage()) <= 0) {
+				//this.activated = false;
+				System.out.println("3");
+				par1ItemStack.stackSize--;
+			}
+			tag.setLong("time", par2World.getTotalWorldTime());
 		}
-		tag.setLong("time", par2World.getTotalWorldTime());
         return par1ItemStack;
     }
-	
-		/*if (this.isNew == false) {
-			par1ItemStack.setItemDamage(100);
-			this.isNew = true;
-		}
-		if (par1ItemStack.getItemDamage() > 0 && this.activated == true) {
-			((EntityLiving)par3Entity).addPotionEffect(new PotionEffect(13, 10, 0, true));
-			par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() - 1);
-		}else if (par1ItemStack.getItemDamage() <= 0 && this.activated == true) {
-			this.activated = false;
-			par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() + 1);
-		}else{
-			par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() + 1);
-		}*/
-	
 	
 	private NBTTagCompound getTagCompound(ItemStack stack) {
         NBTTagCompound nbt;
