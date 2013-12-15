dada1b5e4c4ef79b58ee6f5fa0aa24529f7213de
Hunter-K13 <hunterk13mc@gmail.com>
2013-12-15 17:29:21 -0500

    .

--- hunterk13/ninjasecrets/items/ItemNinjaSkillVanish.java
+++ hunterk13/ninjasecrets/items/ItemNinjaSkillVanish.java
@@ -1,12 +1,14 @@
 package ninjasecrets.items;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import ninjasecrets.NinjaSecrets;
 
 public class ItemNinjaSkillVanish extends ItemNinjaSkillBase {
-
+	
 	public ItemNinjaSkillVanish(int id) {
 		super(id);
 		setCreativeTab(NinjaSecrets.ninTab);
@@ -14,9 +16,15 @@ public class ItemNinjaSkillVanish extends ItemNinjaSkillBase {
 		setUnlocalizedName(ItemInfo.VanishUnlocal);
 	}
 	
-	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer player) {
-		player.worldObj.spawnParticle("slime", player.posX, player.posY, player.posZ, 0, 0 , 0);
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player) {
+		world.createExplosion(player, player.posX, player.posY, player.posZ, 0, false);
+		player.addPotionEffect(new PotionEffect(14, 70, 255, true));
+		player.addPotionEffect(new PotionEffect(18, 70, 127, true));
+		player.hurtResistantTime = 70;
+		if (player.getCurrentEquippedItem().itemID == 261 && player.isUsingItem()) {
+			player.dropOneItem(true);
+		}
+		par1ItemStack.stackSize--;
 		return par1ItemStack;
 	}
-	
 }
